75e2d1726b78a7f5cbec890e49cc97966e6ba9c3
Ilya Ivanov <ilya@devopsx.com>
2024-04-26 23:47:41 +0700

    apparently sudo is necessary

--- gha.sh
+++ gha.sh
@@ -7,12 +7,12 @@ if ! grep "Ubuntu" /etc/issue; then
     exit 0
 fi
 
-rm -f /etc/apt/sources.list.d/microsoft-prod.list
-eatmydata dpkg --add-architecture i386
-eatmydata apt-get update -qq
+sudo rm -f /etc/apt/sources.list.d/microsoft-prod.list
+sudo eatmydata dpkg --add-architecture i386
+sudo eatmydata apt-get update -qq
 
 if [[ "$INPUT_INSTALL_WINE" != 'false' ]]; then
-    DEBIAN_FRONTEND=noninteractive eatmydata apt-get install -yqq --allow-downgrades libc6:i386 libgcc-s1:i386 libstdc++6:i386 wine
+    sudo DEBIAN_FRONTEND=noninteractive eatmydata apt-get install -yqq --allow-downgrades libc6:i386 libgcc-s1:i386 libstdc++6:i386 wine
 
     # wine pollutes the log with "wine: Read access denied for device" if z is linked to /
     if [[ "$INPUT_WINE_FIX_READ_ACCESS_DENIED" != 'false' ]]; then
